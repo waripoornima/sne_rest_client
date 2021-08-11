63b64724b6897d01aed6696579cea375189aacac
waripoornima <62920214+waripoornima@users.noreply.github.com>
2021-08-10 20:49:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
+import os, sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+def main():
+    setup(
+        name='sne_rest_client',
+        version= '0.1',
+        author='PoornimaWari',
+        author_email='poornima.wari@spirent.com',
+        url='https://github.com/waripoornima/sne_rest_client',
+        description='sne_rest_client: Front end for SPirent Network Emulator ReST API',
+        long_description = 'See https://github.com/waripoornima/sne_rest_client',
+        license='http://www.opensource.org/licenses/mit-license.php',
+        keywords='Spirent Network Emulator API',
+        classifiers=['Development Status :: 1 - Production/Stable',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Software Development :: Libraries',
+                     'Topic :: Utilities',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3'],
+        packages=['sne_rest_client'],
+        install_requires=['requests>=2.7'],
+        zip_safe=True,
+        )
 
-setuptools.setup(
-    name='sne_rest_client',
-    version='0.1',
-    author='Poornima Wari',
-    author_email='poornima.wari@spirent.com',
-    description='Installation of Spirent Network Emulator ReST client',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/waripoornima/sne_rest_client',
-    project_urls = {
-        "Bug Tracker": "https://github.com/waripoornima/sne_rest_client/issues"
-    },
-    license='MIT',
-    packages=['sne_rest_client'],
-    install_requires=['requests'],
-)
+
+if __name__ == '__main__':
+    main()
